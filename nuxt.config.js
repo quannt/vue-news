dc274f2d20fa26d91f8fea918180cc6c025c7153
Quan <simpleplan.vn@gmail.com>
2018-12-20 23:51:27 +0700

    Change page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Vue News - A news reader powered by Nuxt.js and NewsAPI.org',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
